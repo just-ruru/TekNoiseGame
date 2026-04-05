cc17b537b4d0426d5951d5f36b6a7ef4e7dd95b9
JustRuru <justruru@users.noreply.github.com>
2026-04-06 05:02:37 +0800

    fixed the tiles and adjusted solid Area width

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -27,7 +27,7 @@ public class Player extends Entity{
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 45;
-        solidArea.width = 32;
+        solidArea.width = 28;
         solidArea.height = 32;
 
         setDefaultValues();
