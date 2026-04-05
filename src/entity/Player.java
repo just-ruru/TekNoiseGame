fcaebaff6d7bd7ca643e5d8f3e17f6f2a827e1d5
JustRuru <justruru@users.noreply.github.com>
2026-04-06 03:36:59 +0800

    fixed something I gues

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -41,11 +41,11 @@ public class Player extends Entity{
         direction = "down";
     }
 
-    public void getPlayerImage(){
+    public void getPlayerImage() {
 
-        switch(choice){
+        switch (choice) {
             case 1:
-                try{
+                try {
                     up1 = ImageIO.read(getClass().getResourceAsStream("/player/Lerler/Up/cabo_up_1.png"));
                     up2 = ImageIO.read(getClass().getResourceAsStream("/player/Lerler/Up/cabo_up_2.png"));
                     up3 = ImageIO.read(getClass().getResourceAsStream("/player/Lerler/Up/cabo_up_3.png"));
@@ -65,12 +65,12 @@ public class Player extends Entity{
                     right2 = ImageIO.read(getClass().getResourceAsStream("/player/Lerler/Right/cabo_right_2.png"));
                     right3 = ImageIO.read(getClass().getResourceAsStream("/player/Lerler/Right/cabo_right_3.png"));
                     right4 = ImageIO.read(getClass().getResourceAsStream("/player/Lerler/Right/cabo_right_4.png"));
-                }catch(IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 break;
             case 2:
-                try{
+                try {
                     up1 = ImageIO.read(getClass().getResourceAsStream("/player/Gasha/Up/gasha_up_1.png"));
                     up2 = ImageIO.read(getClass().getResourceAsStream("/player/Gasha/Up/gasha_up_2.png"));
                     up3 = ImageIO.read(getClass().getResourceAsStream("/player/Gasha/Up/gasha_up_3.png"));
@@ -90,12 +90,12 @@ public class Player extends Entity{
                     right2 = ImageIO.read(getClass().getResourceAsStream("/player/Gasha/Right/gasha_right_2.png"));
                     right3 = ImageIO.read(getClass().getResourceAsStream("/player/Gasha/Right/gasha_right_3.png"));
                     right4 = ImageIO.read(getClass().getResourceAsStream("/player/Gasha/Right/gasha_right_4.png"));
-                }catch(IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 break;
             case 3:
-                try{
+                try {
                     up1 = ImageIO.read(getClass().getResourceAsStream("/player/Nnyl/Up/rayos_up_1.png"));
                     up2 = ImageIO.read(getClass().getResourceAsStream("/player/Nnyl/Up/rayos_up_2.png"));
                     up3 = ImageIO.read(getClass().getResourceAsStream("/player/Nnyl/Up/rayos_up_3.png"));
@@ -115,13 +115,13 @@ public class Player extends Entity{
                     right2 = ImageIO.read(getClass().getResourceAsStream("/player/Nnyl/Right/rayos_right_2.png"));
                     right3 = ImageIO.read(getClass().getResourceAsStream("/player/Nnyl/Right/rayos_right_3.png"));
                     right4 = ImageIO.read(getClass().getResourceAsStream("/player/Nnyl/Right/rayos_right_4.png"));
-                }catch(IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 break;
 
             case 4:
-                try{
+                try {
                     up1 = ImageIO.read(getClass().getResourceAsStream("/player/Gemal/Up/gemal_up_1.png"));
                     up2 = ImageIO.read(getClass().getResourceAsStream("/player/Gemal/Up/gemal_up_2.png"));
                     up3 = ImageIO.read(getClass().getResourceAsStream("/player/Gemal/Up/gemal_up_3.png"));
@@ -141,10 +141,11 @@ public class Player extends Entity{
                     right2 = ImageIO.read(getClass().getResourceAsStream("/player/Gemal/Right/gemal_right_2.png"));
                     right3 = ImageIO.read(getClass().getResourceAsStream("/player/Gemal/Right/gemal_right_3.png"));
                     right4 = ImageIO.read(getClass().getResourceAsStream("/player/Gemal/Right/gemal_right_4.png"));
-                }catch(IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 break;
+        }
     }
 
     public void update(){
@@ -201,94 +202,93 @@ public class Player extends Entity{
         }
     }
 
-    public void draw(Graphics2D g2){
+    public void draw(Graphics2D g2) {
 //        g2.setColor(Color.RED);
 //        g2.fillOval(x, y, gp.tileSize, gp.tileSize);
 
         BufferedImage image = null;
 
-        switch(direction){
+        switch (direction) {
             case "up":
-                if(spriteNum == 1){
+                if (spriteNum == 1) {
                     image = up1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = up2;
                 }
-                if(spriteNum == 3){
+                if (spriteNum == 3) {
                     image = up3;
                 }
-                if(spriteNum == 4){
+                if (spriteNum == 4) {
                     image = up4;
                 }
                 break;
 
             case "down":
-                if(spriteNum == 1){
+                if (spriteNum == 1) {
                     image = down1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = down2;
                 }
-                if(spriteNum == 3){
+                if (spriteNum == 3) {
                     image = down3;
                 }
-                if(spriteNum == 4){
+                if (spriteNum == 4) {
                     image = down4;
                 }
                 break;
 
             case "left":
-                if(spriteNum == 1){
+                if (spriteNum == 1) {
                     image = left1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = left2;
                 }
-                if(spriteNum == 3){
+                if (spriteNum == 3) {
                     image = left3;
                 }
-                if(spriteNum == 4){
+                if (spriteNum == 4) {
                     image = left4;
                 }
                 break;
 
             case "right":
-                if(spriteNum == 1){
+                if (spriteNum == 1) {
                     image = right1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = right2;
                 }
-                if(spriteNum == 3){
+                if (spriteNum == 3) {
                     image = right3;
                 }
-                if(spriteNum == 4){
+                if (spriteNum == 4) {
                     image = right4;
                 }
                 break;
         }
 
-        switch(choice) {
+        switch (choice) {
             case 1:
                 g2.drawImage(image, screenX, screenY, 25 * 2, 40 * 2, null);
                 break;
 
             case 2:
-                g2.drawImage(image, screenX, screenY, 29*2, 32*2, null);
+                g2.drawImage(image, screenX, screenY, 29 * 2, 32 * 2, null);
                 break;
 
             case 3:
-                g2.drawImage(image, screenX, screenY, 25*2, 41*2, null);
+                g2.drawImage(image, screenX, screenY, 25 * 2, 41 * 2, null);
                 break;
 
             case 4:
-                g2.drawImage(image, screenX, screenY, 31*2, 37*2, null);
+                g2.drawImage(image, screenX, screenY, 31 * 2, 37 * 2, null);
                 break;
             default:
                 break;
         }
-
-
     }
 }
+
